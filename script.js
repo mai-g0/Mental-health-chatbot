67a50dc509872df0794dc1bab445d1836c884fd6
Yvonne Maigo <Cynthiayvonne07@gmail.com>
2026-06-07 13:27:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+  const chatBox = document.getElementById ('chat-box');
+  const userInput = document.getElementById('user-input');
+  const sendButton = document.getElementById('send-button');
+</script>
